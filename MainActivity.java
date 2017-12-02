8f5ecb2ff1a5ef9ba5f2d2f31d99c3d707b679d0
Aabed Kashwa <kashwa80@gmail.com>
2017-12-02 13:27:23 +0200

    Uploading files.

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.wordpress.aabedsite.coffeshop;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
